2e1641c0bb82e8ab3e83989e629ee435da72b5fc
Hugh <gamehaxe@gmail.com>
2014-04-01 23:59:28 +0800

    Look as splitting off dock renderer

---
+++ gm2d/skin/DockRenderer.hx
@@ -0,0 +1,52 @@
+package gm2d.skin;
+
+import gm2d.ui.Widget;
+import gm2d.ui.WidgetState;
+import gm2d.ui.IDockable;
+import gm2d.ui.Pane;
+import gm2d.ui.HitBoxes;
+import nme.geom.Rectangle;
+import nme.display.Sprite;
+import nme.display.Graphics;
+
+
+class DockRenderer
+{
+   public var gripperTop:Bool;
+   public var tallDock:Bool;
+
+   public function new(inTall:Bool)
+   {
+      tallDock = inTall;
+      gripperTop = inTall;
+   }
+
+   public function getResizeBarWidth() : Float { return Skin.current.getResizeBarWidth(); }
+   public function getChromeRect(inDocked:IDockable) : Rectangle
+   {
+      return Skin.current.getChromeRect(inDocked,gripperTop);
+   }
+   public function renderResizeBar(outDisplay:Sprite, inRect:Rectangle, inPos:Float)
+   {
+      var gfx = outDisplay.graphics;
+      var gap = getResizeBarWidth();
+      var extra = 2;
+      gfx.beginFill(Skin.current.panelColor);
+      if (tallDock)
+         gfx.drawRect(inRect.x, inRect.y+inPos,inRect.width,gap);
+      else
+         gfx.drawRect(inRect.x+inPos, inRect.y,gap,inRect.height);
+   }
+   public function renderPaneChrome(outDisplay:Sprite, inPane:Pane, inRect:Rectangle,
+                outHitBoxes:HitBoxes )
+   {
+      Skin.current.renderPaneChrome(inPane,outDisplay,outHitBoxes,inRect,
+              gripperTop?Skin.TOOLBAR_GRIP_TOP:0);
+   }
+   public function renderToolbarGap(outDisplay:Sprite, x:Float, y:Float, w:Float, h:Float)
+   {
+      Skin.current.renderToolbarGap(outDisplay,x,y,w,h);
+   }
+
+}
+
