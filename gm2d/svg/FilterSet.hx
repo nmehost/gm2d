51c513521b2e0475b37e0e59683330069be28563
Hugh Sanderson <gamehaxe@gmail.com>
2020-08-29 17:55:56 +0800

    Add FilterSet

---
+++ gm2d/svg/FilterSet.hx
@@ -0,0 +1,17 @@
+package gm2d.svg;
+
+import nme.geom.Rectangle;
+import nme.filters.*;
+
+class FilterSet
+{
+   // Rect?
+   public var filters:Array<BitmapFilter>;
+
+   public function new()
+   {
+      filters = [];
+   }
+}
+
+
