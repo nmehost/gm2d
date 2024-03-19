da8742627cbf1ba94312bc04b2fafa7441cca8e9
Hugh Sanderson <gamehaxe@gmail.com>
2024-03-19 14:04:02 +0800

    Add some optional fields to the Progress Dialog

--- gm2d/ui/ProgressDialog.hx
+++ gm2d/ui/ProgressDialog.hx
@@ -5,25 +5,54 @@ import gm2d.ui.Button;
 class ProgressDialog extends Dialog
 {
    var progress:ProgressBar;
+   var statusCtrl:TextLabel;
+   var button:Button;
 
-   function new(inPane:Pane,inProgress:ProgressBar) { super(inPane); progress = inProgress; }
+   function new(inPane:Pane,inProgress:ProgressBar, inStatus:TextLabel, inButton:Button)
+   {
+      super(inPane);
+      progress = inProgress;
+      statusCtrl = inStatus;
+      button = inButton;
+   }
 
-   public static function create(inTitle:String, inLabel:String, inMax:Float, ?inOnCancel:Void->Void)
+   public static function create(inTitle:String, inLabel:String, ?statusText:String, inMax:Float, ?inOnCancel:Void->Void)
    {
       var panel = new Panel(inTitle);
 
-      panel.addLabel(inLabel);
-      var progress = new ProgressBar(inMax);
-      panel.addUI(progress);
+      var status = statusText==null ? null : new TextLabel(statusText);
+      if (status!=null)
+         panel.addLabelUI("Status", status);
 
+      var progress = new ProgressBar(inMax);
+      panel.addLabelUI(inLabel, progress);
+
+      var button:Button = null;
       if (inOnCancel!=null)
-         panel.addButton( Button.TextButton("Cancel", inOnCancel ) );
+         panel.addButton( button = Button.TextButton("Cancel", inOnCancel ) );
+
+
 
       panel.setSizeHint(500);
 
-      return new ProgressDialog(panel.getPane(),progress);
+      return new ProgressDialog(panel.getPane(),progress, status, button);
    }
 
+   public function setStatus(text:String)
+   {
+      if (statusCtrl!=null)
+         statusCtrl.setText(text);
+   }
+
+   public function setButton(text:String, ?onClick:Void->Void)
+   {
+      if (button!=null)
+      {
+         button.setText(text);
+         if (onClick!=null)
+            button.mCallback = onClick;
+      }
+   }
 
    public function update(inValue:Float)
    {
